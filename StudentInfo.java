6ee2a62e2aba77f1fa2655dfd7c9c09ef5d04d5f
reareacrumcrum <126303254+reareacrumcrum@users.noreply.github.com>
2023-09-24 17:18:32 -0500

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,20 @@
+/**
+* Simple program to print name and previous comp classes. 
+* 
+* Activity 1
+* Reagan Crumley - COMP-1210-001
+* 1/16/2023
+*/
+public class StudentInfo
+{
+/**
+* Prints name and none for previous COMP classes.
+* @param args Command line arguments - not used.
+*/
+   public static void main(String[] args)
+   {
+      System.out.println("Name: Reagan Crumley");
+      System.out.println("Previous Computer Courses:");
+      System.out.println("   None");
+   }
+}
